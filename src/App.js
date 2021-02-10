e3e80432182a40ff28a044ee21efa9386ce5ca95
kabir-webDev <kabir.webdev@gmail.com>
2021-02-10 17:22:59 +0600

    ReBuild the footer and the navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Docs from "./components/Docs";
 import Features from "./components/Features";
@@ -8,10 +9,23 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/features">
+            <Features />
+          </Route>
+          <Route path="/docs">
+            <Docs />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
